80e1bfedcbba13177dfdd7a91298cb220e4c3787
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-07-06 18:00:38 +0200

    BONUS - Degree 2 - Delta neg - B - Not a - Not sqrt

--- bonus/degree_2_neg.py
+++ bonus/degree_2_neg.py
@@ -885,8 +885,65 @@ def	delta_neg_b_nota_notsqrt(var, b, delta_sqrt, p) :
 	str2 = str_b + " + " + __utils__.ft_round(delta_sqrt, 0) + "i"
 	if b == int(b) :
 		return str1, str2
-	print("TODO")
-	return "", ""
+	i1 = " - " + __utils__.ft_round(delta_sqrt, 0) + "i"
+	i2 = " + " + __utils__.ft_round(delta_sqrt, 0) + "i"
+	str1 = str_b + i1
+	str2 = str_b + i2
+	if b == int(b) :
+		return str1, str2
+	mult = __bonus__.irreducible_mult(b, 1, p)
+	b_num = int(b * mult)
+	b_den = int(mult)
+	print()
+	print("<=>	" + var + "_1 = " + __utils__.ft_round(b_num, 0) + " / " + __utils__.ft_round(b_den, 0) + i1)
+	print("	\33[33mor\033[32m")
+	print("	" + var + "_2 = " + __utils__.ft_round(b_num, 0) + " / " + __utils__.ft_round(b_den, 0) + i2)
+	primes1 = __bonus__.primes(b_num)
+	primes2 = __bonus__.primes(b_den)
+	delete = []
+	__bonus__.reduce_fraction(primes1, primes2, delete)
+	num = __bonus__.print_frac(primes1, delete, True)
+	den = __bonus__.print_frac(primes2, delete, False)
+	if len(num) > 1 or len(den) > 1 :
+		print()
+		print("<=>	" + var + "_1 = " + num + " / " + den + i1)
+		print("	\33[33mor\033[32m")
+		print("	" + var + "_2 = " + num + " / " + den + i2)
+		__bonus__.fraction_delete(primes1, delete.copy())
+		__bonus__.fraction_delete(primes2, delete.copy())
+		str_num = __bonus__.print_frac(primes1, None, True)
+		str_den = __bonus__.print_frac(primes2, None, False)
+		print()
+		print("<=>	" + var + "_1 = " + str_num + " / " + str_den + i1)
+		print("	\33[33mor\033[32m")
+		print("	" + var + "_2 = " + str_num + " / " + str_den + i2)
+		if len(primes1) > 1 or len(primes2) > 1 :
+			num, den = __bonus__.irreducible(primes1, primes2)
+			print()
+			print("<=>	" + var + "_1 = " + __utils__.ft_round(num, 0) + " / " + __utils__.ft_round(den, 0) + i1)
+			print("	\33[33mor\033[32m")
+			print("	" + var + "_2 = " + __utils__.ft_round(num, 0) + " / " + __utils__.ft_round(den, 0) + i2)
+		else :
+			num = 1 if not primes1 else primes1[0]
+			den = 1 if not primes2 else primes2[0]
+	else :
+		num = b_num
+		den = b_den
+	str1 = __utils__.ft_round(num, 0) + " / " + __utils__.ft_round(den, 0) + i1
+	str2 = __utils__.ft_round(num, 0) + " / " + __utils__.ft_round(den, 0) + i2
+	arround = __utils__.ft_round(num / den, 15)
+	print("\033[35m")
+	if len(arround[arround.find('.') + 1:]) < 15 :
+		print("<=>	" + var + "_1 = " + arround + i1)
+		print("	\33[33mor\033[35m")
+		print("	" + var + "_2 = " + arround + i2)
+		str1 = arround + i1
+		str2 = arround + i2
+	else :
+		print("<=>	" + var + "_1 ≈ " + arround + i1)
+		print("	\33[33mor\033[35m")
+		print("	" + var + "_2 ≈ " + arround + i2)
+	return str1, str2
 
 def	delta_neg_b_nota(var, b, delta, p) :
 	str_b = __utils__.ft_round(b, p)
