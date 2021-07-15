caeb0f8907aaab94ed29c7331df1614bb791bdd4
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-07-15 20:12:50 +0200

    BONUS - Degree 2 - Delta neg - B - A (Beginning)

--- bonus/degree2/neg/b/degree2_neg_b.py
+++ bonus/degree2/neg/b/degree2_neg_b.py
@@ -6,7 +6,7 @@
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/14 15:04:11 by qpupier           #+#    #+#              #
-#    Updated: 2021/07/15 19:51:19 by qpupier          ###   ########lyon.fr    #
+#    Updated: 2021/07/15 20:12:25 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -438,8 +438,26 @@ def	delta_neg_b_nota(var, b, delta, p) :
 		den = 1
 	return delta_neg_b_nota_sqrt(var, num, den, delta, p)
 
+def	delta_neg_b_a_sqrt(var, a, b, delta, delta_sqrt, p) :
+	str_b = __utils__.ft_round(b, p)
+	print()
+	print("<=>	" + var + "_1 = (" + str_b + " - " + __utils__.ft_round(delta_sqrt, p) + "i) / " + __utils__.ft_round(a, p))
+	print("	\33[33mor\033[32m")
+	print("	" + var + "_2 = (" + str_b + " + " + __utils__.ft_round(delta_sqrt, p) + "i) / " + __utils__.ft_round(a, p))
+	print("TODO NOW")
+	return "", ""
+
+def	delta_neg_b_a_notsqrt(var, a, b, delta, p) :
+	print("TODO")
+	return "", ""
+
+def	delta_neg_b_a(var, a, b, delta, p) :
+	delta_sqrt = __utils__.ft_sqrt(delta)
+	if delta_sqrt == int(delta_sqrt) :
+		return delta_neg_b_a_sqrt(var, a, b, delta, delta_sqrt, p)
+	return delta_neg_b_a_notsqrt(var, a, b, delta, p)
+
 def	delta_neg_b(var, a, b, delta, p) :
 	if a == 1 :
 		return delta_neg_b_nota(var, b, delta, p)
-	print("TODO NOW")
-	return "", ""
+	return delta_neg_b_a(var, a, b, delta, p)
