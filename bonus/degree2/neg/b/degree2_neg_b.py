f8b8d58f23d6aaa8ad82e1312e870a396f195095
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-07-19 16:40:24 +0200

    BONUS - Degree 2 - Delta neg - B - A - Sqrt

--- bonus/degree2/neg/b/degree2_neg_b.py
+++ bonus/degree2/neg/b/degree2_neg_b.py
@@ -6,7 +6,7 @@
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/14 15:04:11 by qpupier           #+#    #+#              #
-#    Updated: 2021/07/19 16:39:24 by qpupier          ###   ########lyon.fr    #
+#    Updated: 2021/07/19 16:40:14 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -942,8 +942,6 @@ def delta_neg_b_a_sqrt_apos(var, a, b, delta_sqrt, p) :
 		b = tmp_b
 		return delta_neg_b_a_sqrt_apos_bint(var, a, b, delta_sqrt, p)
 	return delta_neg_b_a_sqrt_apos_notbint(var, a, b, delta_sqrt, p)
-	print("TODO NOW") # Trouver dans quel cas on peut rentrer ici
-	return "", ""
 
 def	delta_neg_b_a_sqrt(var, a, b, delta_sqrt, p) :
 	str_b = __utils__.ft_round(b, p)
@@ -956,7 +954,7 @@ def	delta_neg_b_a_sqrt(var, a, b, delta_sqrt, p) :
 	return delta_neg_b_a_sqrt_apos(var, a, b, delta_sqrt, p)
 
 def	delta_neg_b_a_notsqrt(var, a, b, delta, p) :
-	print("TODO")
+	print("TODO NOW") # Trouver dans quel cas on peut rentrer ici
 	return "", ""
 
 def	delta_neg_b_a(var, a, b, delta, p) :
