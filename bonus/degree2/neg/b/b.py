72d8b2d6503e1d9204af1707bc272e9cec405883
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-07-25 16:09:35 +0200

    BONUS - Degree 2 - Delta neg - B - A

--- bonus/degree2/neg/b/b.py
+++ bonus/degree2/neg/b/b.py
@@ -6,7 +6,7 @@
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/14 15:04:11 by qpupier           #+#    #+#              #
-#    Updated: 2021/07/25 16:09:00 by qpupier          ###   ########lyon.fr    #
+#    Updated: 2021/07/25 16:09:23 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -2929,14 +2929,14 @@ def	delta_neg_b_a_notsqrt(var, a, b, delta, p) :
 	if a < 0 :
 		return delta_neg_b_a_notsqrt_aneg(var, a, b, delta, p)
 	return delta_neg_b_a_notsqrt_apos(var, a, b, delta, p)
-	print("TODO NOW") # Trouver dans quel cas on peut rentrer ici
-	return "", ""
 
 def	delta_neg_b_a(var, a, b, delta, p) :
 	delta_sqrt = __utils__.ft_sqrt(delta)
 	if delta_sqrt == int(delta_sqrt) :
 		return delta_neg_b_a_sqrt(var, a, b, delta_sqrt, p)
 	return delta_neg_b_a_notsqrt(var, a, b, delta, p)
+	print("TODO NOW") # Trouver dans quel cas on peut rentrer ici
+	return "", ""
 
 def	delta_neg_b(var, a, b, delta, p) :
 	if a == 1 :
