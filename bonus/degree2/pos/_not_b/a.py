a5eeb560d7433a1cf5d87750e54247271e09785b
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-07-27 17:44:01 +0200

    BONUS - Degree 2 - Delta pos - Not b - A - Not sqrt - Not delta (Beginning)

--- bonus/degree2/pos/_not_b/a.py
+++ bonus/degree2/pos/_not_b/a.py
@@ -6,16 +6,25 @@
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/07/26 19:27:48 by qpupier           #+#    #+#              #
-#    Updated: 2021/07/27 17:34:56 by qpupier          ###   ########lyon.fr    #
+#    Updated: 2021/07/27 17:43:19 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 import utils as __utils__
 from bonus import utils as __bonus__
 
-def	delta_pos_notb_a_notsqrt(var, delta, a, p) :
+def	delta_pos_notb_a_notsqrt_notdelta(var, delta, a, p) :
 	print("TODO NOW")
 	return "", ""
+
+def	delta_pos_notb_a_notsqrt_delta(var, delta, sqrt_delta, a) :
+	print("TODO")
+	return "", ""
+
+def	delta_pos_notb_a_notsqrt(var, delta, sqrt_delta, a, p) :
+	if delta == int(delta) :
+		return delta_pos_notb_a_notsqrt_delta(var, delta, sqrt_delta, a)
+	return delta_pos_notb_a_notsqrt_notdelta(var, delta, a, p)
 
 def	delta_pos_notb_a_sqrt(var, delta, a, p) :
 	print("TODO")
@@ -25,4 +34,4 @@ def	delta_pos_notb_a(var, a, delta, p) :
 	sqrt_delta = __utils__.ft_sqrt(delta)
 	if sqrt_delta == int(sqrt_delta) :
 		return delta_pos_notb_a_sqrt(var, sqrt_delta, a, p)
-	return delta_pos_notb_a_notsqrt(var, sqrt_delta, a, p)
+	return delta_pos_notb_a_notsqrt(var, delta, sqrt_delta, a, p)
