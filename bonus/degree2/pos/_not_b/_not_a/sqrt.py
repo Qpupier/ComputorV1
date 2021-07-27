ec03e12ea0dab59f902d784eaba57033c400d06c
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-07-27 16:45:18 +0200

    BONUS - Degree 2 - Delta pos - Not b - Not a

--- bonus/degree2/pos/_not_b/_not_a/sqrt.py
+++ bonus/degree2/pos/_not_b/_not_a/sqrt.py
@@ -6,7 +6,7 @@
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/07/26 19:35:26 by qpupier           #+#    #+#              #
-#    Updated: 2021/07/27 16:44:10 by qpupier          ###   ########lyon.fr    #
+#    Updated: 2021/07/27 16:45:05 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,5 +20,3 @@ def	delta_pos_notb_nota_sqrt(var, sqrt_delta) :
 	print("	\33[33mor\033[32m")
 	print("	" + var + "_2 = " + str2)
 	return str1, str2
-	print("TODO NOW")
-	return "", ""
