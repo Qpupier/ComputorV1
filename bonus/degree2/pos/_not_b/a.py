81451585da06ff4664e7664b647f1476408ac2f1
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-07-29 17:51:45 +0200

    BONUS - Degree 2 - Delta pos - Not b - A - Not sqrt

--- bonus/degree2/pos/_not_b/a.py
+++ bonus/degree2/pos/_not_b/a.py
@@ -6,7 +6,7 @@
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/07/26 19:27:48 by qpupier           #+#    #+#              #
-#    Updated: 2021/07/29 17:50:05 by qpupier          ###   ########lyon.fr    #
+#    Updated: 2021/07/29 17:51:33 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -308,13 +308,13 @@ def	delta_pos_notb_a_notsqrt_delta(var, delta, a, p) :
 	if not squares :
 		return delta_pos_notb_nota_notsqrt_notdelta_notdeltaden_notsquares(var, delta_num, a, p)
 	return delta_pos_notb_nota_notsqrt_notdelta_notdeltaden_squares(var, delta_num, squares, a, p)
-	print("TODO NOW")
-	return "", ""
 
 def	delta_pos_notb_a_notsqrt(var, delta, sqrt_delta, a, p) :
 	if delta == int(delta) :
 		return delta_pos_notb_a_notsqrt_delta(var, delta, a, p)
 	return delta_pos_notb_a_notsqrt_notdelta(var, delta, a, p)
+	print("TODO NOW")
+	return "", ""
 
 def	delta_pos_notb_a_sqrt(var, delta, a, p) :
 	print("TODO")
