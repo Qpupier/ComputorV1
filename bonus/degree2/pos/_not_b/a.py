3ec93ff991e7db6d802867b54f51b2cac3dd2836
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-07-29 17:39:10 +0200

    BONUS - Degree 2 - Delta pos - Not b - A - Not sqrt - Not delta - Delta den

--- bonus/degree2/pos/_not_b/a.py
+++ bonus/degree2/pos/_not_b/a.py
@@ -6,7 +6,7 @@
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/07/26 19:27:48 by qpupier           #+#    #+#              #
-#    Updated: 2021/07/29 17:25:20 by qpupier          ###   ########lyon.fr    #
+#    Updated: 2021/07/29 17:37:18 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,6 +14,28 @@ import utils as __utils__
 from bonus import utils as __bonus__
 
 def	delta_pos_notb_nota_notsqrt_notdelta_deltaden(var, delta_num, delta_den_sqrt, a, p) :
+	str_num = __utils__.ft_round(delta_num, 0)
+	str_den = __utils__.ft_round(delta_den_sqrt, 0)
+	str_a = __utils__.ft_round(a, p)
+	print()
+	print("<=>	" + var + "_1 = -√" + str_num + " / " + str_den + " / " + str_a)
+	print("	\33[33mor\033[32m")
+	print("	" + var + "_2 = √" + str_num + " / " + str_den + " / " + str_a)
+	print()
+	print("<=>	" + var + "_1 = -√" + str_num + " / (" + str_den + " * " + str_a + ")")
+	print("	\33[33mor\033[32m")
+	print("	" + var + "_2 = √" + str_num + " / (" + str_den + " * " + str_a + ")")
+	a *= delta_den_sqrt
+	str_a = __utils__.ft_round(a, p)
+	print()
+	print("<=>	" + var + "_1 = -√" + str_num + " / " + str_a)
+	print("	\33[33mor\033[32m")
+	print("	" + var + "_2 = √" + str_num + " / " + str_a)
+	squares, delta_num = __bonus__.reduce_sqrt(delta_num)
+	str_num = __utils__.ft_round(delta_num, 0)
+	if not squares :
+		return delta_pos_notb_nota_notsqrt_notdelta_notdeltaden_notsquares(var, delta_num, a, p)
+	return delta_pos_notb_nota_notsqrt_notdelta_notdeltaden_squares(var, delta_num, squares, a, p)
 	print("TODO NOW")
 	return "", ""
 
