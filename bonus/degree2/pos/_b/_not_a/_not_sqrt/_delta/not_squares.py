47882f48834109311ee8b2f5a839dcc0d5442342
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-08-17 16:46:14 +0200

    BONUS - Degree 2 - Delta pos - B - Not a - Not sqrt - Delta - Not squares (Beginning)

--- bonus/degree2/pos/_b/_not_a/_not_sqrt/_delta/not_squares.py
+++ bonus/degree2/pos/_b/_not_a/_not_sqrt/_delta/not_squares.py
@@ -1,19 +1,15 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    not_sqrt.py                                        :+:      :+:    :+:    #
+#    not_squares.py                                     :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/08/17 15:15:28 by qpupier           #+#    #+#              #
-#    Updated: 2021/08/17 16:37:44 by qpupier          ###   ########lyon.fr    #
+#    Created: 2021/08/17 16:40:01 by qpupier           #+#    #+#              #
+#    Updated: 2021/08/17 16:41:55 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-from bonus.degree2.pos._b._not_a._not_sqrt import not_delta as __not_delta__
-from bonus.degree2.pos._b._not_a._not_sqrt import delta as __delta__
-
-def	delta_pos_b_nota_notsqrt(var, b, delta, sqrt_delta, p) :
-	if delta == int(delta) :
-		return __delta__.delta_pos_notb_nota_notsqrt_delta(var, b, delta, sqrt_delta, p)
-	return __not_delta__.delta_pos_notb_nota_notsqrt_notdelta(var, b, delta, p)
+def	delta_pos_notb_nota_notsqrt_delta_notsquares(var, b, delta, sqrt_delta, p) :
+	print("TODO NOW")
+	return "", ""
---
+++ bonus/degree2/pos/_b/_not_a/_not_sqrt/_delta/squares.py
@@ -0,0 +1,58 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    squares.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/08/17 16:39:58 by qpupier           #+#    #+#              #
+#    Updated: 2021/08/17 16:41:46 by qpupier          ###   ########lyon.fr    #
+#                                                                              #
+# **************************************************************************** #
+
+def	delta_pos_notb_nota_notsqrt_delta_squares(var, b, squares, delta_num, p) :
+	print("TODO")
+	# str_num = __utils__.ft_round(delta_num, 0)
+	# sq = __bonus__.print_squares(squares, delta_num)
+	# print()
+	# print("<=>	" + var + "_1 = -√" + sq)
+	# print("	\33[33mor\033[32m")
+	# print("	" + var + "_2 = √" + sq)
+	# if len(squares) > 1 :
+	# 	tmp = 1
+	# 	for each in squares :
+	# 		tmp *= each * each
+	# 	print()
+	# 	print("<=>	" + var + "_1 = -√(" + __utils__.ft_round(tmp, 0) + " * " + str_num + ")")
+	# 	print("	\33[33mor\033[32m")
+	# 	print("	" + var + "_2 = √(" + __utils__.ft_round(tmp, 0) + " * " + str_num + ")")
+	# else :
+	# 	tmp = squares[0] * squares[0]
+	# print()
+	# print("<=>	" + var + "_1 = -√" + __utils__.ft_round(tmp, 0) + "√" + str_num)
+	# print("	\33[33mor\033[32m")
+	# print("	" + var + "_2 = √" + __utils__.ft_round(tmp, 0) + "√" + str_num)
+	# squares = __utils__.ft_sqrt(tmp)
+	# str_squares = __utils__.ft_round(squares, 0)
+	# str1 = "-" + str_squares + "√" + str_num
+	# str2 = str_squares + "√" + str_num
+	# print()
+	# print("<=>	" + var + "_1 = " + str1)
+	# print("	\33[33mor\033[32m")
+	# print("	" + var + "_2 = " + str2)
+	# arround = __utils__.ft_round(squares * __utils__.ft_sqrt(delta_num), 14)
+	# print("\033[35m")
+	# if len(arround[arround.find('.') + 1:]) < 14 :
+	# 	print("<=>	" + var + "_1 = -" + arround)
+	# 	print("	\33[33mor\033[35m")
+	# 	print("<=>	" + var + "_2 = " + arround)
+	# 	str1 = "-" + arround
+	# 	str2 = arround
+	# else :
+	# 	print("<=>	" + var + "_1 ≈ -" + arround)
+	# 	print("	\33[33mor\033[35m")
+	# 	print("<=>	" + var + "_2 ≈ " + arround)
+	# 	str1 = "-" + str_squares + "√" + __utils__.ft_round(delta_num, 0)
+	# 	str2 = str_squares + "√" + __utils__.ft_round(delta_num, 0)
+	# return str1, str2
+	return "", ""
