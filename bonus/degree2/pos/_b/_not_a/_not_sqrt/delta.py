1c95496d563695d669b4312086a72dd65c607624
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-08-17 16:30:10 +0200

    BONUS - Degree 2 - Delta pos - B - Not a - Not sqrt - Delta (Beginning)

--- bonus/degree2/pos/_b/_not_a/_not_sqrt/delta.py
+++ bonus/degree2/pos/_b/_not_a/_not_sqrt/delta.py
@@ -1,19 +1,15 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    not_sqrt.py                                        :+:      :+:    :+:    #
+#    delta.py                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/08/17 15:15:28 by qpupier           #+#    #+#              #
-#    Updated: 2021/08/17 16:28:36 by qpupier          ###   ########lyon.fr    #
+#    Created: 2021/08/17 16:26:35 by qpupier           #+#    #+#              #
+#    Updated: 2021/08/17 16:28:48 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-from bonus.degree2.pos._b._not_a._not_sqrt import not_delta as __not_delta__
-from bonus.degree2.pos._b._not_a._not_sqrt import delta as __delta__
-
-def	delta_pos_b_nota_notsqrt(var, b, delta, sqrt_delta, p) :
-	if delta == int(delta) :
-		return __delta__.delta_pos_notb_nota_notsqrt_delta(var, b, delta, sqrt_delta)
-	return __not_delta__.delta_pos_notb_nota_notsqrt_notdelta(var, b, delta, p)
+def	delta_pos_notb_nota_notsqrt_delta(var, b, delta, sqrt_delta) :
+	print("TODO NOW")
+	return "", ""
--- bonus/degree2/pos/_b/_not_a/_not_sqrt/not_delta.py
+++ bonus/degree2/pos/_b/_not_a/_not_sqrt/not_delta.py
@@ -1,19 +1,15 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    not_sqrt.py                                        :+:      :+:    :+:    #
+#    not_delta.py                                       :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/08/17 15:15:28 by qpupier           #+#    #+#              #
-#    Updated: 2021/08/17 16:28:36 by qpupier          ###   ########lyon.fr    #
+#    Created: 2021/08/17 16:26:33 by qpupier           #+#    #+#              #
+#    Updated: 2021/08/17 16:28:49 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-from bonus.degree2.pos._b._not_a._not_sqrt import not_delta as __not_delta__
-from bonus.degree2.pos._b._not_a._not_sqrt import delta as __delta__
-
-def	delta_pos_b_nota_notsqrt(var, b, delta, sqrt_delta, p) :
-	if delta == int(delta) :
-		return __delta__.delta_pos_notb_nota_notsqrt_delta(var, b, delta, sqrt_delta)
-	return __not_delta__.delta_pos_notb_nota_notsqrt_notdelta(var, b, delta, p)
+def	delta_pos_notb_nota_notsqrt_notdelta(var, b, delta, p) :
+	print("TODO")
+	return "", ""
