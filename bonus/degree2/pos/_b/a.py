60f5604d5f2b73f0f8928e31259dc378eac37041
Quentin PUPIER (Qpupier) <qpupier@student.42lyon.fr>
2021-08-21 18:08:33 +0200

    BONUS - Degree 2 - Delta pos - B - A (Beginning)

--- bonus/degree2/pos/_b/a.py
+++ bonus/degree2/pos/_b/a.py
@@ -6,7 +6,7 @@
 #    By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/08/17 12:06:05 by qpupier           #+#    #+#              #
-#    Updated: 2021/08/17 17:17:54 by qpupier          ###   ########lyon.fr    #
+#    Updated: 2021/08/21 18:07:25 by qpupier          ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,5 +14,5 @@ import utils as __utils__
 from bonus import utils as __bonus__
 
 def	delta_pos_b_a(var, a, b, delta, p) :
-	print("TODO")
+	print("TODO NOW")
 	return "", ""
